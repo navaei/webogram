482961c8741ff44aeca3a4e17db44d3c7b4695c1
Meysam Navaei <meysam.navaei@hotmail.com>
2018-03-19 20:11:36 +0330

    change dhprime code

--- app/js/lib/mtproto.js
+++ app/js/lib/mtproto.js
@@ -485,7 +485,7 @@ angular.module('izhukov.mtproto', ['izhukov.utils'])
             console.log(dT(), 'Verifying DH params')
             var dhPrimeHex = bytesToHex(dhPrime)
             if (g != 3 ||
-                dhPrimeHex !== 'c71caeb9c6b1c9048e6c522f70f13f73980d40238e3e21c14934d037563d930f48198a0aa7c14058229493d22530f4dbfa336f6e0ac925139543aed44cce7c3720fd51f69458705ac68cd4fe6b6b13abdc9746512969328454f18faf8c595f642477fe96bb2a941d5bcd1d4ac8cc49880708fa9b378e3c4f3a9060bee67cf9a4a4a695811051907e162753b56b0f6b410dba74d8a84b2a14b3144e0ef1284754fd17ed950d5965b4b9dd46582db1178d169c6bc465b0d6ff9ca3928fef5b9ae4e418fc15e83ebea0f87fa9ff5eed70050ded2849f47bf959d956850ce929851f0d8115f635b105ee2e4e15d04b2454bf6f4fadf034b10403119cd8e3b92fcc5b') {
+                dhPrimeHex !== 'c150023e2f70db7985ded064759cfecf0af328e69a41daf4d6f01b538135a6f91f8f8b2a0ec9ba9720ce352efcf6c5680ffc424bd634864902de0b4bd6d49f4e580230e3ae97d95c8b19442b3c0a10d8f5633fecedd6926a7f6dab0ddb7d457f9ea81b8465fcd6fffeed114011df91c059caedaf97625f6c96ecc74725556934ef781d866b34f011fce4d835a090196e9a5f0e4449af7eb697ddb9076494ca5f81104a305b6dd27665722c46b60e5df680fb16b210607ef217652e60236c255f6a28315f4083a96791d7214bf64c1df4fd0db1944fb26a2a57031b32eee64ad15a8ba68885cde74a5bfc920f6abf59ba5c75506373e7130f9042da922179251f') {
                 // The verified value is from https://core.telegram.org/mtproto/security_guidelines
                 throw new Error('[MT] DH params are not verified: unknown dhPrime')
             }
